a28ed81a8e985c3e5c6c3e256e7b985b14ed46a1
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-10-30 19:13:52 +0800

    update port 2

--- index.js
+++ index.js
@@ -172,6 +172,8 @@ io.on("connection", (socket) => {
 	socket.on("disconnect", () => {});
 });
 
-server.listen(3001, "0.0.0.0", () => {
+const port = 3000;
+
+server.listen(port, "0.0.0.0", () => {
 	console.log("Server running on port 3001");
 });
